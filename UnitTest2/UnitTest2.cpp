b24d75fc6908a72b2c0f979bacbdb8e03f35a685
Oleksandra Shafarost <oleksandra.shaforost@infiz.khpi.edu.ua>
2024-11-21 20:51:49 +0200

    Add project files.

---
+++ UnitTest2/UnitTest2.cpp
@@ -0,0 +1,63 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "C:\Users\екзу\source\repos\UnitTest2\functionTest\functionTest.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest2
+{
+    TEST_CLASS(UnitTest2)
+    {
+    public:
+        TEST_METHOD(TestMethod1)
+        {
+            double x = 10;
+            int n = 4;
+            double expected = 4;
+
+            double actual = calculateY(x, n);
+
+            Assert::AreEqual(expected, actual); 
+        }
+
+    public:
+        TEST_METHOD(TestMethod2)
+        {
+            double x = 7;
+            int n = 5;
+            double expected = -3;
+
+            double actual = calculateY(x, n);
+
+            Assert::AreEqual(expected, actual); 
+        }
+
+    public:
+        TEST_METHOD(TestMethod3)
+        {
+            double x = -10;
+            int n = 3;
+            double expected = 0; 
+
+            double actual = calculateY(x, n);
+
+            Assert::AreEqual(expected, actual); 
+        }
+
+    public:
+        TEST_METHOD(TestMethod4_ExceptionCheck)
+        {
+            int n = -3; // n ok за умовою в функції n <= 0 
+
+            try {
+                double x = -8;  // не важливо 
+                calculateY(x, n);
+
+                Assert::Fail(L"Parameter 'n' must be greater than zero.");
+            }
+            catch (...) {
+                Assert::IsTrue(true); 
+            }
+        }
+    };
+}
---
+++ functionTest/functionTest.cpp
@@ -0,0 +1,66 @@
+﻿#include <iostream>
+#include <fstream>
+using namespace std;
+
+// Функція для обчислення значення y в залежності від x та n
+double calculateY(double x, int n) {
+    if (n <= 0) {
+        throw std::invalid_argument("Parameter 'n' must be greater than zero.");
+    }
+    if (x < 0) {
+        double sum = 0;
+        for (int i = 1; i <= n - 1; ++i) {
+            for (int j = 1; j <= n; ++j) {
+                sum += (j) / (j * j + j);
+            }
+        }
+        return sum;
+    }
+    else {
+        double sum = 0;
+        for (int i = 1; i <= n - 1; ++i) {
+            sum += i;
+        }
+        return x - sum;
+    }
+}
+
+int main() {
+    double a, b, h;
+    int n;
+    char saveToFile;
+
+    setlocale(LC_ALL, "Ukrainian");
+
+    cout << "Введіть інтервал (a, b): ";
+    cin >> a >> b;
+    cout << "Введіть крок h: ";
+    cin >> h;
+    cout << "Введіть значення n: ";
+    cin >> n;
+    cout << "Чи хочете зберегти результати у файл? (y/n): ";
+    cin >> saveToFile;
+
+    ofstream outFile;
+    if (saveToFile == 'y') {
+        outFile.open("results.txt");
+        if (!outFile) {
+            cerr << "Помилка відкриття файлу для запису!" << endl;
+        }
+    }
+
+    // Обчислення функції y для кожного значення x
+    for (double x = a; x <= b; x += h) {
+        double y = calculateY(x, n);
+        cout << "x = " << x << ", y = " << y << endl;
+        if (outFile) {
+            outFile << "x = " << x << ", y = " << y << endl;
+        }
+    }
+    if (outFile) {
+        outFile.close();
+        cout << "Результати записані у файл 'results.txt'" << endl;
+    }
+
+    return 0;
+}
